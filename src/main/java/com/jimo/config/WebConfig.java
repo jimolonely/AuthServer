b1563c56d5f614abfee8e21f56667d5115eb7436
jimolonely <wp147188@foxmail.com>
2017-12-12 16:00:15 +0800

    add web config cors

---
+++ src/main/java/com/jimo/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.jimo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //允许全部请求跨域
+        registry.addMapping("/**");
+    }
+}
